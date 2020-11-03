bdb98ec678027c3558f10e78ed97e02b3a82437d
cooperspring <71714356+cooperspring@users.noreply.github.com>
2020-11-03 10:45:36 -0800

    PlayerControllerRotation+Sprite
    Added v1 character sprite and temporary rotation method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEditor;
 using Unity.Mathematics;
+using UnityEditorInternal;
 
 public class PlayerController : MonoBehaviour
 {
@@ -33,6 +34,8 @@ public class PlayerController : MonoBehaviour
         //get the input from player
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+       
     }
 
     private void FixedUpdate()
@@ -45,6 +48,68 @@ public class PlayerController : MonoBehaviour
             vertical *= moveLimiter;
         }
 
-        myRB.velocity = new Vector2(horizontal * Speed, vertical * Speed);
+        //set the velocity of the player to the input values
+        myRB.velocity = new Vector2(horizontal * Speed, vertical * Speed).normalized;
+
+        //temporary method of setting direction rotation, may be changed for efficiency
+        //try a switch if it isn't bugging out later (compiler error only in unity)
+        //for the switch, use bool negativePressH/V, PressH/V, and notPressedH/V declared at top, make sure to reset at end of update
+        /*
+         if(horizontal > 0)
+         {
+            PressH = true;
+         }
+         else if(horizontal < 0)
+         {
+            negativePressH = true;
+         }
+         else
+         {
+            notPressedH = true;
+         }
+         
+         And Et Cetera...
+         */
+        if (horizontal > 0 && vertical == 0)
+        {
+            myRB.SetRotation(0);
+        }
+
+        if (horizontal > 0 && vertical > 0)
+        {
+            myRB.SetRotation(45);
+        }
+
+        if (horizontal == 0 && vertical > 0)
+        {
+            myRB.SetRotation(90);
+        }
+
+        if (horizontal < 0 && vertical > 0)
+        {
+            myRB.SetRotation(135);
+        }
+
+        if (horizontal < 0 && vertical == 0)
+        {
+            myRB.SetRotation(180);
+        }
+
+        if (horizontal < 0 && vertical < 0)
+        {
+            myRB.SetRotation(225);
+        }
+
+        if (horizontal == 0 && vertical < 0)
+        {
+            myRB.SetRotation(270);
+        }
+
+        if (horizontal > 0 && vertical < 0)
+        {
+            myRB.SetRotation(315);
+        }
+
+        
     }
 }
