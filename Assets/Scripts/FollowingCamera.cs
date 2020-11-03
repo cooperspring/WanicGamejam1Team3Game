f0dc646f1c39873084533e44a6a6594b9d7bfbe1
cooperspring <71714356+cooperspring@users.noreply.github.com>
2020-11-03 08:25:46 -0800

    Basic Player Test
    Basic test for player controls

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,38 @@
+﻿/////By Cooper Spring
+/////Date : 11/3/2020
+////This is a camera script that is used to follow specified target
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public GameObject Target;
+    public float smoothVal = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(Target != null)
+        {
+            //Determine target location
+            Vector3 newPos = Target.transform.position;
+            //maintain camera z
+            newPos.z = -1f;
+            //use linear interpolation to smooth movement to target
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothVal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿/////By Cooper Spring
+/////Date : 11/3/2020
+////This is a movement script to move the player with input, using tank controls for now
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEditor;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 200;
+    public float TurnSpeed = 10f;
+    Rigidbody2D myRB;
+    //Vector3 startingUp;
+    //Vector3 startingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        //startingUp = transform.up;
+        //startingRight = transform.right;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //set up stored variable for movement
+        Vector2 movement = new Vector2();
+        
+        //check for input and use that to move 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //get mouse position as world point
+        //Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //get pointer direction
+        //Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        //set player up vector to smoothed direction
+        //transform.up += Vector3.Lerp(transform.up, direction, TurnSpeed);
+        
+        //Set forward and backward speeds
+        myRB.AddForce(transform.up * movement.y * Speed * Time.deltaTime);
+        myRB.AddTorque((-movement.x) * TurnSpeed * Time.deltaTime);
+
+        //myRB.AddForce(startingRight * movement.x * Speed * Time.deltaTime);
+
+        
+    }
+}
